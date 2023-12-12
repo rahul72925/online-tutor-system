19d46f08ce6c192c8496e3a782fc94ae92386bb9
rahul72925 <rahulkhatri72925@gmail.com>
2023-12-12 20:15:48 +0530

    feat: end point form delete, get-all and single classroom

--- entities/classrooms/controllers.js
+++ entities/classrooms/controllers.js
@@ -1,4 +1,5 @@
 import SQL from "../../sql.js";
+import { isStudent, isTutor } from "../../utils/index.js";
 
 const createClassroom = async (req, res) => {
   try {
@@ -68,11 +69,12 @@ const updateClassroom = async (req, res) => {
         classroom_id: classroomId,
         student_id: eachId,
       }));
+      // link classroom with student || if student already link with classroom then do nothing
       await SQL`INSERT INTO "onlineTutorSystem"."classrooms_students" ${SQL(
         mappedDataWithClassroomId,
         "classroom_id",
         "student_id"
-      )}`;
+      )} ON CONFLICT (classroom_id, student_id) DO NOTHING;`;
     }
     res.status(200).json({
       success: true,
@@ -103,4 +105,132 @@ const updateClassroom = async (req, res) => {
   }
 };
 
-export { createClassroom, updateClassroom };
+const deleteClassroom = async (req, res) => {
+  try {
+    const { classroomId } = req.params;
+
+    if (!classroomId) {
+      throw new Error("CLASSROOM_ID_UNAVAILABLE");
+    }
+    await SQL`UPDATE "onlineTutorSystem"."classrooms" set is_archived = true where id=${classroomId};`;
+    return res.status(200).json({
+      success: true,
+      message: "classroom deleted successfully",
+    });
+  } catch (error) {
+    if (error.message === "CLASSROOM_ID_UNAVAILABLE") {
+      return res.status(401).json({
+        success: false,
+        error: "classroomId required",
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+};
+
+const getAllClassrooms = async (req, res) => {
+  try {
+    const { userType, tutorId, studentId } = req;
+
+    if (isTutor(userType)) {
+      //get all the classrooms of a tutor
+      const allClassRoomsCreatedByTutor =
+        await SQL`SELECT * FROM "onlineTutorSystem"."classrooms" where tutor_id = ${tutorId} and is_archived =false;`;
+
+      return res.status(200).json({
+        success: true,
+        data: {
+          classrooms: allClassRoomsCreatedByTutor,
+        },
+      });
+    }
+    if (isStudent(userType)) {
+      const studentClassrooms = await SQL`
+      SELECT oc.* from "onlineTutorSystem"."classrooms" oc 
+      LEFT JOIN "onlineTutorSystem"."classrooms_students" ocs 
+      ON oc.id = ocs."classroom_id" 
+      where oc."is_archived" = false and ocs."student_id" = ${studentId};
+      `;
+      return res.status(200).json({
+        success: true,
+        data: {
+          classrooms: studentClassrooms,
+        },
+      });
+    }
+    throw new Error("INSUFFICIENT_DATA");
+  } catch (error) {
+    if (error.message === "INSUFFICIENT_DATA") {
+      return res.status(401).json({
+        success: false,
+        error: "userType not available",
+      });
+    } else {
+      console.log("get classroom error", error);
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+};
+
+const getClassroom = async (req, res) => {
+  try {
+    const { userType, studentId, tutorId } = req;
+    const { classroomId } = req.params;
+    if (!userType) throw new Error("INSUFFICIENT_DATA");
+
+    if (!classroomId) throw new Error("CLASSROOM_ID_UNAVAILABLE");
+
+    let classroom = [];
+    if (isTutor(userType)) {
+      classroom =
+        await SQL`SELECT * FROM "onlineTutorSystem"."classrooms" where is_archived=false and tutor_id = ${tutorId} and id=${classroomId};`;
+    }
+    if (isStudent(userType)) {
+      classroom = await SQL`
+      SELECT oc.* from "onlineTutorSystem"."classrooms" oc 
+      LEFT JOIN "onlineTutorSystem"."classrooms_students" ocs 
+      ON oc.id = ocs."classroom_id" 
+      where oc."is_archived" = false and ocs."student_id" = ${studentId} and oc.id = ${classroomId};
+      `;
+    }
+    return res.status(200).json({
+      success: true,
+      data: {
+        classroom,
+      },
+    });
+  } catch (error) {
+    if (error.message === "INSUFFICIENT_DATA") {
+      return res.status(401).json({
+        success: false,
+        error: "userType not available",
+      });
+    } else if (error.message === "CLASSROOM_ID_UNAVAILABLE") {
+      return res.status(401).json({
+        success: false,
+        error: "classroomId not available",
+      });
+    } else {
+      console.log(error.message);
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+};
+
+export {
+  createClassroom,
+  updateClassroom,
+  deleteClassroom,
+  getAllClassrooms,
+  getClassroom,
+};
--- entities/classrooms/routes.js
+++ entities/classrooms/routes.js
@@ -1,10 +1,24 @@
 import express from "express";
 import { authorize, authorizedToTutor } from "../../middlewares/index.js";
-import { createClassroom, updateClassroom } from "./controllers.js";
+import {
+  createClassroom,
+  deleteClassroom,
+  updateClassroom,
+  getAllClassrooms,
+  getClassroom,
+} from "./controllers.js";
 
 const router = express.Router();
 
 router.post("/create", authorize, authorizedToTutor, createClassroom);
 router.post("/update", authorize, authorizedToTutor, updateClassroom);
+router.post(
+  "/delete/:classroomId",
+  authorize,
+  authorizedToTutor,
+  deleteClassroom
+);
+router.get("/get", authorize, getAllClassrooms);
+router.get("/get/:classroomId", authorize, getClassroom);
 
 export default router;
