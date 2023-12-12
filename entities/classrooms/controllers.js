76c54172df922cfa4de4664d27d42be603ffe8a4
rahul72925 <rahulkhatri72925@gmail.com>
2023-12-12 19:12:12 +0530

    feat: end point for update classroom

--- entities/classrooms/controllers.js
+++ entities/classrooms/controllers.js
@@ -2,7 +2,7 @@ import SQL from "../../sql.js";
 
 const createClassroom = async (req, res) => {
   try {
-    const { name, description, linkedStudentIds = [] } = req.body;
+    const { name, description, toBeLinkStudentIds = [] } = req.body;
     if (!name || !description) {
       throw new Error("NAME_DESCRIPTION_ERROR");
     }
@@ -11,8 +11,8 @@ const createClassroom = async (req, res) => {
       await SQL`INSERT INTO "onlineTutorSystem"."classrooms" (name, description, "tutor_id")
     values(${name},${description},${tutorId}) returning id;`;
 
-    if (linkedStudentIds.length > 0) {
-      await SQL`INSERT INTO "onlineTutorSystem"."classrooms_students" (classroom_id,student_id) values ${linkedStudentIds.map(
+    if (toBeLinkStudentIds.length > 0) {
+      await SQL`INSERT INTO "onlineTutorSystem"."classrooms_students" (classroom_id,student_id) values ${toBeLinkStudentIds.map(
         (eachId, index) => SQL`(${newClassroom[0].id},${eachId})`
       )};`;
     }
@@ -37,4 +37,70 @@ const createClassroom = async (req, res) => {
     });
   }
 };
-export { createClassroom };
+
+const updateClassroom = async (req, res) => {
+  try {
+    const {
+      name = null,
+      description = null,
+      toBeLinkStudentIds = [],
+      classroomId,
+    } = req.body;
+    if (!classroomId) {
+      throw new Error("CLASSROOM_ID_NOT_AVAILABLE");
+    }
+
+    if (!name && !description && toBeLinkStudentIds.length === 0) {
+      throw new Error("NOT_DATA_TO_UPDATE");
+    }
+    const updatedInfo = {};
+    if (name !== null) updatedInfo.name = name;
+    if (description !== null) updatedInfo.description = description;
+    let updatedClassroom = null;
+    if (name || description) {
+      updatedClassroom =
+        await SQL`UPDATE "onlineTutorSystem"."classrooms" set ${SQL(
+          updatedInfo
+        )} , updated_at = now() where id=${classroomId} returning *;`;
+    }
+    if (toBeLinkStudentIds.length > 0) {
+      const mappedDataWithClassroomId = toBeLinkStudentIds.map((eachId) => ({
+        classroom_id: classroomId,
+        student_id: eachId,
+      }));
+      await SQL`INSERT INTO "onlineTutorSystem"."classrooms_students" ${SQL(
+        mappedDataWithClassroomId,
+        "classroom_id",
+        "student_id"
+      )}`;
+    }
+    res.status(200).json({
+      success: true,
+      error: null,
+      data: {
+        updatedClassroom,
+        studentLinked: toBeLinkStudentIds.length,
+      },
+    });
+  } catch (error) {
+    if (error.message === "CLASSROOM_ID_NOT_AVAILABLE") {
+      return res.status(401).json({
+        status: false,
+        error: "classroomId is required",
+      });
+    } else if (error.message === "NOT_DATA_TO_UPDATE") {
+      return res.status(401).json({
+        status: false,
+        error: "no data available for update",
+      });
+    } else {
+      console.log("error", error);
+      return res.status(500).json({
+        status: false,
+        error: error.message,
+      });
+    }
+  }
+};
+
+export { createClassroom, updateClassroom };
--- entities/classrooms/routes.js
+++ entities/classrooms/routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { authorize, authorizedToTutor } from "../../middlewares/index.js";
-import { createClassroom } from "./controllers.js";
+import { createClassroom, updateClassroom } from "./controllers.js";
 
 const router = express.Router();
 
 router.post("/create", authorize, authorizedToTutor, createClassroom);
+router.post("/update", authorize, authorizedToTutor, updateClassroom);
 
 export default router;
